968b4604c744eab375ffd856e19de2cf6d865208
toffer <cristopher.moralesceron@outlook.com>
2019-01-17 12:34:09 -0300

    Creacion de SucursalDao

---
+++ src/main/java/cl/accenture/programatufuturo/proyectofinal/inventario/dao/SucursalDAO.java
@@ -0,0 +1,25 @@
+package cl.accenture.programatufuturo.proyectofinal.inventario.dao;
+
+public class SucursalDAO {
+    //Se crea atributo para la conexion, dado que sera necesaria para trabajar con los datos y tablas de SQL
+    private Conexion conexion;
+
+    //Inicializo esta clase con un constructor que permita la conexion.
+
+    //CONSTRUCTOR
+    public SucursalDAO(){
+
+        this.conexion=new Conexion();
+    }
+
+    // GETTERS Y SETTERS
+
+    public Conexion getConexion() {
+
+        return this.conexion;
+    }
+    public void setConexion(Conexion conexion) {
+
+        this.conexion = conexion;
+    }
+}
