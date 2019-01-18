60ee42e48b800dd12ad7eca02168734def840848
Carolyns-Lagos <carolyns.echegaray@gmail.com>
2019-01-17 22:21:51 -0300

    Modificacion entidad-relacion

--- src/main/java/cl/accenture/programatufuturo/proyectofinal/inventario/dao/SQLIdAutoincrementable.java
+++ src/main/java/cl/accenture/programatufuturo/proyectofinal/inventario/dao/SQLIdAutoincrementable.java
@@ -97,5 +97,38 @@ public class SQLIdAutoincrementable{
         return id;
     }
 
+    public int idIncrementableVenta() throws SinConexionException, SQLException{
+        //Inicializo las variables necesarias.
+        int id=1;
+        PreparedStatement ps= null;
+        ResultSet rs=null;
+        try {
+            final String SQL = "SELECT MAX(idVenta) FROM inventarioPF.Venta" ;
+            ps=conexion.obtenerConnection().prepareStatement(SQL);
+            rs=ps.executeQuery();
+            //Recorro la tabla, pisiendo el valor de la columna 1 y a ese valor sumarle 1
+            while (rs.next()){
+                id=rs.getInt(1)+1;
+            }
+            //En caso de haber un error en la consulta, se indicara
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            //En caso de algun error se indicara
+        } catch (Exception ex){
+            System.out.println("Error"+ ex.getMessage());
+
+            //Finally se ocupara para cuando se terminen de ocupar las conecciones mara evitar que consuma mas memoria de la necesaria en el equipo
+        } finally {
+            try {
+                ps.close();
+                rs.close();
+            }catch (Exception ex){
+
+            }
+        }
+        return id;
+    }
+
 
 }
